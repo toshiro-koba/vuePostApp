b87e6661db6d10653ba14aa3fe5dd6cc5f195220
toshiro-koba <kobato.231.0625@gmail.com>
2022-05-18 00:31:27 +0900

    リストの雛形を作成

---
+++ src/components/Persons.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import PersonPostForm from './PersonPostForm.vue';
+import PersonList from './PersonList.vue';
+</script>
+
+<template>
+  <div class="container">
+    <PersonPostForm />
+    <div class="list-container">
+      <ul>
+        <PersonList />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
